44c954b19daf7813c60fb7a125a1816d41e2dd46
Mohamed <looklike882@gmail.com>
2025-04-01 23:01:32 +0000

    [Add] Testimonial component wrapped in SectionWrapper

---
+++ app/components/Testimonial.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SectionWrapper from "../Hoc"
+const Testimonial = () => {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default SectionWrapper({Children:Testimonial,id:"testimonial"})
